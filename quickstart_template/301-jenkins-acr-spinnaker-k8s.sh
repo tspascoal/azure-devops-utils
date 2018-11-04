cd3c8cf9f99823ea902733756e169bc1a14ee8b8
Tiago Pascoal <tiago@pascoal.net>
2018-11-04 18:12:41 +0000

    Update 301-jenkins-acr-spinnaker-k8s.sh

--- quickstart_template/301-jenkins-acr-spinnaker-k8s.sh
+++ quickstart_template/301-jenkins-acr-spinnaker-k8s.sh
@@ -84,8 +84,8 @@ function install_az() {
     echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
     sudo tee /etc/apt/sources.list.d/azure-cli.list
     curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-    sudo apt-get update
-    sudo apt-get install apt-transport-https azure-cli
+    sudo apt-get --yes update
+    sudo apt-get --yes install apt-transport-https azure-cli
   fi
 }
 
